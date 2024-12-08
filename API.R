845281cd15ded4a310b81e0e1bc0380a65d69652
rmferrand <rmferran@ncsu.edu>
2024-12-08 01:37:09 -0500

    functionally complete

--- API.R
+++ API.R
@@ -1,89 +1,72 @@
 #myAPI.R 
 
+library(plumber)
+
+# Load required libraries
+library(parsnip)
+library(dplyr)
+
 rf_full_fit <- rf_final_wkf |>
   fit(diabetes_final)
 
+final_rf_model <- extract_fit_parsnip(rf_full_fit)
+# Get the predictors used
+print(final_rf_model$fit$forest$independent.variable.names)
 
-library(GGally)
-library(leaflet)
 
-#Send a message
-#* @get /readme
-function(){
-  "This is our basic API"
+
+#* Prediction Endpoint
+#' Predict outcomes using the trained model.
+#' Accepts predictors as query parameters, with default values.
+#' @param GenHlth General health rating
+#' @param MentHlth Mental health condition
+#' @param PhysHlth Physical health condition
+#' @param Sex Gender
+#' @param Age Age
+#' @param Education Highest level of education
+#' @param Income Income bracket
+#' @post /pred
+
+function(AnyHealthcare = "1", 
+         NoDocbcCost = "0",
+         GenHlth = "Very Good", 
+         MentHlth = "No Pain", 
+         PhysHlth = "No Pain", 
+         Sex = "Female", 
+         Age = "60-64",
+         Education = "College graduate", 
+         Income = ">=$75,000") {
+  
+  default_diabetes <- data.frame(
+    AnyHealthcare = factor(AnyHealthcare, levels = c(0, 1), labels = c("0", "1")),
+    NoDocbcCost = factor(NoDocbcCost, levels = c(0, 1), labels = c("0", "1")),
+    GenHlth = factor(GenHlth, levels = c(1:5), labels = c("Excellent", "Very Good", "Good", "Fair", "Poor"),ordered = TRUE),
+    MentHlth = factor(MentHlth, levels = c(1:3), labels = c("No Pain", "Some Pain", "Everyday Pain"), ordered = TRUE),
+    PhysHlth = factor(PhysHlth, levels = c(1:3), labels = c("No Pain", "Pain", "Everyday Pain"), ordered = TRUE),
+    Sex = factor(Sex, levels = c(1:2), labels = c("Female", "Male")),
+    Age = factor(Age, levels = c(1:13), labels = c("18-24", "25-29", "30-34", "35-39", "40-44", "45-49", 
+                                                   "50-54", "55-59", "60-64", "65-69", "70-74", "75-79", 
+                                                   "80-99"), ordered = TRUE),
+    Education = factor(Education, levels = c(1:6), labels = c("NoSchool", "Elementary", "Some high school", 
+                                                              "High school graduate", 
+                                                              "Some college or technical school", 
+                                                              "College graduate"), ordered = TRUE),
+    Income = factor(Income, levels = c(1:8), labels = c("<$10,000",
+                                                        "$10,000-$14,999",
+                                                        "$15,000-$19,999",
+                                                        "$20,000-$24,999",
+                                                        "$25,000 - $34,999",
+                                                        "$35,000 - $49,999",
+                                                        "$50,000 - $74,999",
+                                                        ">=$75,000"), ordered = TRUE)
+    )
+  
+  prediction <- predict(rf_full_fit, default_diabetes, type = "class")
+  
+  list(prediction = as.character(prediction$.pred_class))
 }
 
-#http://localhost:PORT/readme
-
-
-#* Find natural log of a number
-#* @param num Number to find ln of
-#* @get /ln
-function(num){
-  log(as.numeric(num))
-}
-
-#query with http://localhost:PORT/ln?num=1
-
-#* Find multiple of two numbers
-#* @param num1 1st number
-#* @param num2 2nd number
-#* @get /mult
-function(num1, num2){
-  as.numeric(num1)*as.numeric(num2)
-}
-
-#query with http://localhost:PORT/mult?num1=10&num2=20
-
-#* Plot of iris data
-#* @serializer png
-#* @param type base or ggally
-#* @param color TRUE or FALSE (only for ggally)
-#* @get /plotiris
-function(type = "base", color = FALSE){
-  if(tolower(type) == "ggally"){
-    if(color){
-      a <- GGally::ggpairs(iris, aes(color = Species))
-      print(a)
-    } else {
-      a <- GGally::ggpairs(iris)
-      print(a)
-    }
-  } else {
-    pairs(iris)
-  }
-}
-#http://localhost:PORT/plotiris?type=ggally
-
-
-#* Plotting widget
-#* @serializer htmlwidget
-#* @param lat latitude
-#* @param lng longitude
-#* @get /map
-function(lng = 174.768, lat = -36.852){
-  m <- leaflet::leaflet() |>
-    addTiles() |>  # Add default OpenStreetMap map tiles
-    addMarkers(as.numeric(lng), as.numeric(lat))
-  m  # Print the map
-}
-
-#query with http://localhost:PORT/map?lng=174&lat=-36
-
-
-# Choose a predictor
-#* @param predictor
-#* @get /pred
-function(predictor) {
-  data <- iris
-  if (is.numeric(data[[predictor]])) {
-    value <- mean(data[[predictor]])
-    message <- paste("The mean of", predictor, "is", value)
-    return(message)
-  } else if (predictor == "Species") {
-    table <- table(data[[predictor]])
-    return(paste0(names(table), ": ", table))
-  } else {
-    stop("Invalid predictor.")
-  }
-}
+# Example API test calls
+# curl -X POST -H "Content-Type: application/json" -d '{"GenHlth": "Very Good", "MentHlth": "No Pain", "PhysHlth": "No Pain", "Sex": "Female", "Education": "College graduate", "Income": ">= $75,000"}' http://localhost:8000/pred
+# curl -X POST -H "Content-Type: application/json" -d '{}' http://localhost:8000/pred
+# curl -X POST -H "Content-Type: application/json" -d '{"GenHlth": "Excellent"}' http://localhost:8000/pred
